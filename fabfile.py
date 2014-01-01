12ca0f9f40abbf668cf3fd484627f5e365f3a72c
Jeff Schenck <jmschenck@gmail.com>
2013-12-31 20:01:51 -0800

    Fixes redis sorting in redis >=2.6

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,56 @@
 import os
+import re
 
-from fabric.api import local
-from fabric.context_managers import hide, settings
+from fabric.api import hide, lcd, local, prompt, settings
+
+
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 def test():
     """Runs the blingalytics test suite."""
-    path = os.path.dirname(os.path.realpath(__file__))
-    os.environ['PYTHONPATH'] = path
+    os.environ['PYTHONPATH'] = PROJECT_PATH
     with settings(hide('warnings'), warn_only=True):
         local('python test/test_runner.py')
 
 
 def update_pypi():
     """Updates versions and packages for PyPI."""
-    # - update version in setup.py
-    # - update version in docs/conf.py
-    # - git tag -a vx.y.z -m "vx.y.z"
-    # - git push origin master --tags
-    # - python setup.py register
-    # - python setup.py sdist upload
-    #     (cannot be in vagrant shared directory -- copy contents to tmp)
+    # Verify that we want to do this...
+    sure = prompt('Are you sure you want to release a new version to PyPI? '
+        'Have you pushed all changes to origin/master? [y/n]',
+        validate=r'^[yYnN]')
+    if sure.lower()[0] != 'y':
+        return
+
+    # First update version numbers
+    with lcd(PROJECT_PATH):
+        old_version = local('grep version= setup.py', capture=True)
+    old_version = re.search(r'\'([0-9a-zA-Z.]+)\'', old_version).group(1)
+    new_version = prompt(
+        'What version number (previous: {0})?'.format(old_version),
+        validate=r'^\d+\.\d+\.\d+\w*$')
+    local('sed -i -r -e "s/{before}/{after}/g" {filename}'.format(
+        filename=os.path.join(PROJECT_PATH, 'setup.py'),
+        before=r"version='[0-9a-zA-Z.]+'",
+        after='version={0}'.format(new_version)))
+    local('sed -i -r -e "s/{before}/{after}/g" {filename}'.format(
+        filename=os.path.join(PROJECT_PATH, 'docs', 'conf.py'),
+        before=r"^version = '\d+\.\d+'$",
+        after="version = '{0}'".format('.'.join(new_version.split('.')[:2]))))
+    local('sed -i -r -e "s/{before}/{after}/g" {filename}'.format(
+        filename=os.path.join(PROJECT_PATH, 'docs', 'conf.py'),
+        before=r"^release = '\d+\.\d+\.\w+'$",
+        after="release = '{0}'".format(new_version)))
+
+    # Then tag and push to git
+    local('git tag -a v{0} -m "v{0}"'.format(new_version))
+    local('git push origin master --tags')
+
+    # Register new version on PyPI
+    # Note: copy to /tmp because vagrant shared directories don't handle
+    #       links well, which are part of the sdist process
+    local('cp -r {0} /tmp/'.format(os.path.join(PROJECT_PATH, 'blingalytics')))
+    with lcd('/tmp/blingalytics'):
+        local('python setup.py register')
+        local('python setup.py sdist upload')
