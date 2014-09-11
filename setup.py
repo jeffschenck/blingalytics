51dcd86a6134d6aca8b8f93e2bd21501e8e371f3
vagrant <vagrant@blingalytics>
2014-09-11 21:42:38 +0000

    Revs version to v0.2.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ open-source project. Blingalytics is released under the `MIT License`_.
 
 setup(
     name='Blingalytics',
-    version='0.2.1',
+    version='0.2.2',
     author='Jeff Schenck, Adly Inc.',
     author_email='jmschenck@gmail.com',
     url='http://github.com/jeffschenck/blingalytics',
